e7abfe661a70279f362b9a2f576d1437d0cbc4a1
s1040486 <s1040486@mail.ncyu.edu.tw>
2016-04-26 23:16:41 +0800

    Update test.c

--- test.c
+++ test.c
@@ -7,58 +7,41 @@ extern char *getStudentID(char *id);
 
 int check1(char m)
 {
-	if (m >= '0' && m <= '9')
-		return 0;
-
-	return 1;
-}
-
-int check2(char *output)
-{
-	int length;
-
-	length = strlen(output);
-
-	if (length == LEN)
-		return 0;
-	else
-		printf("Check 2 is fail! Student id length is incorrect!\n");
-
-	return 1;
-}
-
-int check3(char *str1, char *str2)
-{
-	int ret;
-
-	ret = (strcmp(str1, str2) != 0);
-
-	if (ret)
-		printf("Check 3 is fail! Do not modify the return statement of getStudentID function!\n");
-
-	return ret;
-}
-
-int main()
-{
-	char id[10];
-	char *output;
-	int ret = 0;
-
-	output = getStudentID(id);
-
-	/* check */
-
-	for (int i = 0; i < LEN; i++) {
-		ret |= check1(output[i]);
-	}
-
-	if (ret)
-		printf("Check 1 is fail! Please give 7 digit numbers!\n");
-
-	ret |= check2(output);
-
-	ret |= check3(output, id);
-
-	return ret;
-}
+  	if (m >= '0' && m <= '9')
+  		return 0;
+ -//
+ -	return 1;
+ +
+ +	return 1;//
+  }
+  
+  int check2(char *output)
+ @@ -23,7 +23,7 @@ int check2(char *output)
+  		return 0;
+  	else
+  		printf("Check 2 is fail! Student id length is incorrect!\n");
+ -
+ +//
+  	return 1;
+  }
+  
+ @@ -35,7 +35,7 @@ int check3(char *str1, char *str2)
+  
+  	if (ret)
+  		printf("Check 3 is fail! Do not modify the return statement of getStudentID function!\n");
+ -
+ +//
+  	return ret;
+  }
+  
+ @@ -48,7 +48,7 @@ int main()
+  	output = getStudentID(id);
+  
+  	/* check */
+ -
+ +//
+  	for (int i = 0; i < LEN; i++) {
+  		ret |= check1(output[i]);
+  	}
+Status API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
